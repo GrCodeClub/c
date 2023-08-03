d12cef2c04f2d886dd73fc2ac57f5a499bd61b4d
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-03 15:50:08 +0300

    Update Δημιουργία_Socket.c

--- Δίκτυα και Διαδίκτo/Δημιουργία_Socket.c
+++ Δίκτυα και Διαδίκτo/Δημιουργία_Socket.c
@@ -4,10 +4,6 @@
 #include <winsock2.h>
 
 int main() {
-    // Αλλαγή της κωδικοποίησης σε 1253 ("Windows-1253" ή "Greek Windows")
-    // system("chcp 1253"); 
-    // Αλλαγή της κωδικοποίησης σε UTF-8
-    system("chcp 65001");
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         fprintf(stderr, "WSAStartup failed.\n");
